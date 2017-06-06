54af4cb79004536538a2d344ae29666021813891
Jesus Montero <jesusdmonteroc@outlook.com>
2017-06-06 09:14:19 -0500

    agregando funcionalidad del formulario

---
+++ lib/formulario.php
@@ -0,0 +1,24 @@
+<?php
+                $email_to = "jesusdmonteroc@outlook.com";
+
+                $email_subject = "Interesado en pautar EO";
+
+                $email_from = "jesusdmonteroc@outlook.com";
+
+                $time = time();
+
+            $email_message = "Fecha y hora: ".date("d/m/Y (H:i:s)", $time)."\n".
+            "Nombre: ".$_POST["nombre"]."\n".
+            "Email: ".$_POST["email"]."\n".
+            "Teléfono: ".$_POST["telefono"]."\n".
+            "Ciudad: ".$_POST["ciudad"]."\n".
+            "Empresa: ".$_POST["empresa"]."\n".
+              "Productos de interés: ".$_POST["selectProduct"]."\n".
+              "Comentarios: ".$_POST["comentarios"]."\n".
+            "Página anterior: ".$_POST["pagina"];
+
+            $headers = 'From: '.$email_from."\r\n".
+            'Reply-To: '.$email_from."\r\n" .
+            'X-Mailer: PHP/' . phpversion();
+
+            @mail($email_to, $email_subject, $email_message, $headers);
